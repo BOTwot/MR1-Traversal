775dd413b6d84799a8b6889c611e1cb7116ad453
Kushal <kushshah777888@gmail.com>
2019-01-22 11:54:43 +0530

    Added without Gyro Mode

---
+++ Without_gyro_mode/GyroRead.h
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+class GyroRead
+{
+  private:
+    float angle;
+    uint8_t _addr;
+    float rtvalue;
+  public:
+    void begin(int addr)
+    {
+      _addr = addr;
+      Wire.begin();
+      Wire.beginTransmission(_addr);
+    }
+    float getAngle()
+    {
+      if (Wire.requestFrom(_addr, 4) == 4)
+      I2C_readAnything (angle);
+      
+      return angle;
+    }
+};
